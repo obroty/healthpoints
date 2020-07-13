638544895598450f9df64a3d9fba6b7559e015e3
Grzegorz Krypien <grzegorzkrypien@gmail.com>
2020-07-13 20:20:16 +0200

    Added http client to fetch data from backend server

--- hp-frontend/src/app/defibrilator.service.ts
+++ hp-frontend/src/app/defibrilator.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 import { DefibInterface } from './defib-interface';
 import { DEFIBS } from './mock-defibs';
@@ -8,9 +9,12 @@ import { DEFIBS } from './mock-defibs';
   providedIn: 'root'
 })
 export class DefibrilatorService {
-  constructor() { }
+  private defibrilatorsUrl = 'http://localhost:8000/api/v1/?format=json'
+
+  constructor(
+    private http: HttpClient) { }
 
   getDefibrilators(): Observable<DefibInterface[]> {
-    return of(DEFIBS);
+    return this.http.get<DefibInterface[]>(this.defibrilatorsUrl);
   }
 }
