3e60ab46076b9253b3717afd64b3ba291230217c
Grzegorz Krypien <grzegorzkrypien@gmail.com>
2020-07-13 18:20:47 +0200

    Fixed DefibInterface and fixed typos in template and mocked data

--- hp-frontend/src/app/mock-defibs.ts
+++ hp-frontend/src/app/mock-defibs.ts
@@ -1,7 +1,7 @@
 import { DefibInterface } from './defib-interface';
 
 export const DEFIBS: DefibInterface[] = [
-  { name: 'Instytut Pamięci Narodowej', addres: 'ul. Janickiego 30' },
-  { name: 'NZOZ. Przychodnie Medycyny Rodzinnej', addres: 'Górna 18b Bezrzecze' },
-  { name: 'Media Markt – Atrium Molo', addres: 'Mieszka I 73' }
+  { name: 'Instytut Pamięci Narodowej', address: 'ul. Janickiego 30' },
+  { name: 'NZOZ. Przychodnie Medycyny Rodzinnej', address: 'Górna 18b Bezrzecze' },
+  { name: 'Media Markt – Atrium Molo', address: 'Mieszka I 73' }
 ];
